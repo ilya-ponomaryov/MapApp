41cea6c5465681abfbf30ab7b33ae56bffbdc9f5
Ilya Ponomaryov <i.ponomaryov.00@gmail.com>
2023-05-25 13:59:46 +0300

    Add navigation

--- app/src/main/java/com/example/mapapp/MainActivity.kt
+++ app/src/main/java/com/example/mapapp/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.mapapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
+
+    private val navigation: NavController
+        get() =
+            Navigation.findNavController(this@MainActivity, R.id.nav_host_fragment)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
