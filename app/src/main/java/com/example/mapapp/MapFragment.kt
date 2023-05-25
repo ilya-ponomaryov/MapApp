d078573cab4c21d7665267bbf963aae167f96e51
Ilya Ponomaryov <i.ponomaryov.00@gmail.com>
2023-05-25 21:48:30 +0300

    Fix import

--- app/src/main/java/com/example/mapapp/MapFragment.kt
+++ app/src/main/java/com/example/mapapp/MapFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mapapp
 
+import com.example.mapapp.common.fragments.BindingFragment
 import com.example.mapapp.databinding.FragmentMapBinding
 
 class MapFragment : BindingFragment<FragmentMapBinding>(
