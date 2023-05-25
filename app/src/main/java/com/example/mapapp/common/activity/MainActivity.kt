b156a7d5796a974945c78e00eb69f967dbaad48a
Ilya Ponomaryov <i.ponomaryov.00@gmail.com>
2023-05-26 00:58:01 +0300

    Add menu in toolbar

--- app/src/main/java/com/example/mapapp/common/activity/MainActivity.kt
+++ app/src/main/java/com/example/mapapp/common/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mapapp.common.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.mapapp.R
@@ -15,5 +16,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.map_menu, menu)
+        return true
     }
 }
