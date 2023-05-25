144588acce6904f0a0a44bd280cb1a143e977bfb
Ilya Ponomaryov <i.ponomaryov.00@gmail.com>
2023-05-25 13:45:16 +0300

    Add viewBinding
    Setting viewBinding

---
+++ app/src/main/java/com/example/mapapp/BindingFragment.kt
@@ -0,0 +1,27 @@
+package com.example.mapapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
+
+abstract class BindingFragment<VB : ViewBinding>(
+    private val inflate: Inflate<VB>
+) : Fragment() {
+    private var _binding: VB? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflate(inflater, container, false)
+            .also { _binding = it }
+            .root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/mapapp/MapFragment.kt
@@ -0,0 +1,8 @@
+package com.example.mapapp
+
+import com.example.mapapp.databinding.FragmentMapBinding
+
+class MapFragment : BindingFragment<FragmentMapBinding>(
+    FragmentMapBinding::inflate
+) {
+}
