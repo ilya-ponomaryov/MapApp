fa060b0cbda87db8f29b6b3110f9582d532e4a81
Ilya Ponomaryov <i.ponomaryov.00@gmail.com>
2023-05-26 14:16:13 +0300

    Add CursorView.kt

---
+++ app/src/main/java/com/example/mapapp/CursorView.kt
@@ -0,0 +1,25 @@
+package com.example.mapapp
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.View
+
+class CursorView(context: Context) : View(context) {
+
+    private val paint = Paint().apply {
+        color = Color.RED
+        strokeWidth = 5f
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.let {
+            val x = width / 2f
+            val y = height / 2f
+            it.drawLine(x - 50, y, x + 50, y, paint)
+            it.drawLine(x, y - 50, x, y + 50, paint)
+        }
+    }
+}
